afa3adbdcf24fca994e03445759c68571aed0cbb
seniordesignws20 <59314252+seniordesignws20@users.noreply.github.com>
2020-01-08 08:30:00 -0600

    Update Configuration.h

--- Marlin-2.0.x/Marlin/Configuration.h
+++ Marlin-2.0.x/Marlin/Configuration.h
@@ -80,8 +80,8 @@
 
   // SCARA tower offset (position of Tower relative to bed zero position)
   // This needs to be reasonably accurate as it defines the printbed position in the SCARA space.
-  #define SCARA_OFFSET_X    -142       // (mm)
-  #define SCARA_OFFSET_Y    -121       // (mm)
+  #define SCARA_OFFSET_X    0       // (mm)
+  #define SCARA_OFFSET_Y    0       // (mm)
 
   #if ENABLED(MORGAN_SCARA)
 
@@ -736,7 +736,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4[, E5]]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 1600, 93 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 1600, 93 }
 
 /**
  * Default Max Feed Rate (mm/s)
